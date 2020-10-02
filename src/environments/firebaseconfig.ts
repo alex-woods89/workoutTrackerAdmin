abb70a339dd506e0067b0803e61f2290be612533
alex-woods89 <52571445+alex-woods89@users.noreply.github.com>
2020-10-02 15:01:38 +0100

    Delete firebaseconfig.ts

--- src/environments/firebaseconfig.ts
+++
@@ -1,8 +0,0 @@
-export const FirebaseConfig = {
-    apiKey: "AIzaSyC0KWzeVdUQYKkh555WcoV0dP_9h7aWpL4",
-    authDomain: "workoutapp-f15a2.firebaseapp.com",
-    databaseURL: "https://workoutapp-f15a2.firebaseio.com",
-    projectId: "workoutapp-f15a2",
-    storageBucket: "workoutapp-f15a2.appspot.com",
-    messagingSenderId: "658087768808"
-  }
